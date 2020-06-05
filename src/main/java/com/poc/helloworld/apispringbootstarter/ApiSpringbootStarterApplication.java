0639131b440016c1f3c77bc482c977ca3592858f
Uday Vemparala <udaykiran@udays-mbp.lan>
2020-06-05 17:01:38 +0800

    Test the circle ci integration

---
+++ src/main/java/com/poc/helloworld/apispringbootstarter/ApiSpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.poc.helloworld.apispringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootStarterApplication.class, args);
+	}
+
+}
